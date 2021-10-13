fbfce318997997b6464e18dd2ac99cf3c87b3144
Roberto <mario24roberto@gmail.com>
2021-10-13 15:01:41 -0600

    Agregando el metodo getEmployee2

--- Frontend/src/app/components/employee/employee.component.ts
+++ Frontend/src/app/components/employee/employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { EmployeeService } from '../../services/employee.service'; 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
--- Frontend/src/app/services/employee.service.ts
+++ Frontend/src/app/services/employee.service.ts
@@ -6,6 +6,8 @@ import { Injectable } from '@angular/core';
 export class EmployeeService {
 
   constructor() { }
+getEmployees(){
+  return[]
+}
 
-  
 }
